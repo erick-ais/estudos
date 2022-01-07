60d7b17084d75f050c6b1fb612f7a64eced62bcc
Erick A. I. Souza <ais.projetostx@gmail.com>
2022-01-07 12:18:07 -0300

    Melhorias em POO

--- Dart/8_poo/heranca.dart
+++ Dart/8_poo/heranca.dart
@@ -1,32 +1,54 @@
 void main() {
   // Dog
-  Dogs cachorro = Dogs();
-  cachorro.nome = 'Thor';
-  cachorro.idade = 7;
-
-  print('Dog: ${cachorro.nome}');
-  print('Idade: ${cachorro.idade}');
+  Dogs cachorro = Dogs('Thor', 7);
+  print(cachorro.toString());
 
   cachorro.latir();
   cachorro.comer();
   cachorro.dormir();
+  cachorro.castrar();
   cachorro.espaco();
 
   // Cat
-  Cats gato = Cats();
-  gato.nome = 'Loki';
-  gato.idade = 6;
-
-  print('Cat: ${gato.nome}');
-  print('Idade: ${gato.idade}');
+  Cats gato = Cats('Loki', 6);
+  print(gato.toString());
 
   gato.miar();
   gato.comer();
   gato.dormir();
+  gato.castrar();
   cachorro.espaco();
+
+  // Ao invés de usar cada uma das classes, é possível usar a super classe
+  List<Animais> animal = [];
+  animal.add(cachorro);
+  animal.add(gato);
+
+  // O "is" verifica se realmente é um dos animais
+  Animais animal1 = funcao();
+  if (animal1 is Dogs) {
+    animal1.latir();
+  } else if (animal1 is Cats) {
+    animal1.miar();
+  }
+
+  // O "as" é o cast que tenta mudar um tipo para outro
+  Cats pet = funcao2() as Cats;
+  pet.miar();
 }
 
-class Animais {
+Animais funcao() {
+  return Dogs('Dog', 2);
+}
+
+Animais funcao2() {
+  return Cats('Batman', 10);
+}
+
+// Super classe
+abstract class Animais {
+  Animais(this.nome, this.idade);
+
   String? nome;
   int? idade;
 
@@ -41,17 +63,44 @@ class Animais {
   void espaco() {
     print('-' * 20);
   }
+
+  @override // O override está sendo no "object"
+  String toString() {
+    return 'Nome: $nome Idade: $idade';
+  }
+
+  // Será preciso específicar em cada clase de Dogs e Cats
+  void castrar(); // Comportamento não devido.
 }
 
 class Dogs extends Animais {
+  Dogs(String nome, int idade) : super(nome, idade);
+  // O "super" está sendo usado como o próprio construtor!
   void latir() {
     print('Au, Au');
+  }
+
+  @override
+  void castrar() {
+    print('Foi castrado!');
   }
 }
 
 class Cats extends Animais {
+  Cats(String nome, int idade) : super(nome, idade);
   void miar() {
     print('Miaaau');
+  }
+
+  // Método reescrito, mas só com mudança em Cats!
+  @override
+  void dormir() {
+    print('Dorme demais!!!');
+  }
+
+  @override
+  void castrar() {
+    print('Não foi castrado.');
   }
 }
 
@@ -59,4 +108,17 @@ class Cats extends Animais {
   HERANÇA
   Ao adicionar "extends Animais" signfica que a classe está herdando a classe
   "Animal", isso é, está herdando atributos e métodos que a classe possui.
+
+  Toda classe criada no dart sempre vai herdar implicitamente uma classe
+  específica que se chama "Object"
+
+  O "@override"
+  Siginifica que o método está fazendo um "override" na super classe
+
+  SUPER
+  Serve para acessar os atributos, métodos, construtores, get e set
+  da super classe.
+
+  ABSTRACT
+  Trona a classe abstrata, assim não é possível instanciar a super classe!
 */
