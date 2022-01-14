b2e617fa730b00bc276f8a9042dcc6ef5b688f4f
Erick A. I. Souza <ais.projetostx@gmail.com>
2022-01-14 08:26:51 -0300

    Novos projetos

--- Flutter/dev/ola_mundo/lib/main.dart
+++ Flutter/dev/ola_mundo/lib/main.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  int valor = 10;
+  runApp(MyApp(
+    title: 'App',
+    valor: valor,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final String title;
+  final int valor;
+  const MyApp({Key? key, required this.title, required this.valor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Aplicativo'),
+        // Cabeçalho
+        appBar: AppBar(
+          title: Text(this.title),
         ),
-        body: const Center(
+        // Corpo
+        body: Center(
           child: Text(
-            'Olá Mundo!',
-            style: TextStyle(fontSize: 50, color: Colors.black),
+            'Olá, Mundo! Valor é ' + this.valor.toString(),
+            // Estilo
+            style: TextStyle(fontSize: 32, color: Colors.black),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+// Stateless: Sem mudança de estado;
+// Stateful: Com mudança de estado
+
+/*
+  Em um Stateful Widget teremos 2 Objetos:
+
+  WIDGET
+  Responsavél por criar o Objeto <State> e guardar dados não mutáveis.
+
+  OBJETO DE ESTADO <State>
+  Responsável por criar o widget com método build e mostrar a variáveis mutáveis
+  e não mutáveis.
+*/
--- Flutter/exemplos/hello_world/lib/main.dart
+++ Flutter/exemplos/hello_world/lib/main.dart
@@ -1,15 +1,4 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text(
-        'App Hello World',
-        style: TextStyle(
-          fontSize: 36,
-          color: Colors.white,
-        ),
-      ),
-    ),
-  ));
 }
