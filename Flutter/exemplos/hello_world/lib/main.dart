02d5059505046956eaa6bff5dc21dfc59d351260
Erick A. I. Souza <ais.projetostx@gmail.com>
2022-01-15 14:53:42 -0300

    Inícios das práticas

--- Flutter/exemplos/hello_world/lib/main.dart
+++ Flutter/exemplos/hello_world/lib/main.dart
@@ -1,4 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text('Aplicativo'),
+      ),
+    );
+  }
 }
