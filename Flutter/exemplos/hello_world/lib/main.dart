383903c1142f15aace86bd66d503e437e745135d
Erick A. I. Souza <ais.projetostx@gmail.com>
2022-01-17 18:25:34 -0300

    Prática do curso básico

--- Flutter/exemplos/hello_world/lib/main.dart
+++ Flutter/exemplos/hello_world/lib/main.dart
@@ -1,6 +1,49 @@
 import 'package:flutter/material.dart';
-import 'myapp.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Aplicativo());
+}
+
+class Aplicativo extends StatelessWidget {
+  const Aplicativo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ErickHome(),
+    );
+  }
+}
+
+class ErickHome extends StatefulWidget {
+  const ErickHome({Key? key}) : super(key: key);
+  @override
+  _ErickHomeState createState() => _ErickHomeState();
+}
+
+class _ErickHomeState extends State<ErickHome> {
+
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu'),
+      ),
+      body: Center(
+        child: Text('Erick A. I. Souza ${count}',
+        textDirection: TextDirection.ltr,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+      ),
+    );
+  }
 }
