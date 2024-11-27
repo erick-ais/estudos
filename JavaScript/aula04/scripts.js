78f429131a35b683447c7f084cf0c084bb19a6b5
Erick A. I. Souza <76917713+erick-ais@users.noreply.github.com>
2024-11-27 15:22:12 -0300

    Novos estudos

---
+++ JavaScript/aula04/scripts.js
@@ -0,0 +1,8 @@
+/**
+ * Scripts das aulas do curso de Javascript.
+ */
+
+// página principal
+window.alert('Minha primneira mensagem!')
+window.confirm('Está gostando de JS?')
+window.prompt('Qual é seu nome?')
