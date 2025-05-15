82c18b084aba1272ac57b283a64144b0d82fb206
Erick A. I. Souza <76917713+erick-ais@users.noreply.github.com>
2025-05-15 17:00:13 -0300

    Atualização

--- JavaScript/aula04/scripts.js
+++
@@ -1,9 +0,0 @@
-/**
- * Scripts das aulas do curso de Javascript.
- */
-
-// página principal
-window.alert('Minha primneira mensagem!')
-window.confirm('Está gostando de JS?')
-window.prompt('Qual é seu nome?')
-
